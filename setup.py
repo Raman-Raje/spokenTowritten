15789a2e17a8a6fb02fd871f754f9f93e8a83f7d
Raman Shinde <raman.shinde15@gmail.com>
2019-10-26 01:31:04 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,17 +3,28 @@ from setuptools import setup
 with open("README", 'r') as f:
   long_description = f.read()
 
+
 setup(
-    name='spoken2written',
-    version='1.0',
-    description="""A module for converting spoken english to written english.""",
-    long_description=long_description,
-    author='Raman Shinde',
-    author_email='raman.shinde15@gmail.com',
-    url="https://github.com/Raman-Raje/spoken2written",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+  name = 'spoken2written',        
+  packages = ['spoken2written'],   # Chose the same as "name"
+  version = '1.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = long_description,   # Give a short description about your library
+  author = 'Raman Shinde',                   # Type in your name
+  author_email = 'raman.shinde15@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/Raman-Raje/spoken2written',   # Provide either the link to your github or to your website
+  #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  install_requires=[            # I get to this in a second
+          'forex-python',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
